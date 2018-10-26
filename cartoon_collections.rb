6a15a6cb826a1b31ab1c4e7dd2fd149a94601e8d
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-26 04:18:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(planets)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
